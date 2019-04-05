862b4ba2b3401b13b86d237aad115c015970a6a9
melmass <melmassadian@gmail.com>
2019-04-05 15:59:13 +0200

    comments got removed during merge

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -20,5 +20,5 @@ PYTHON
 echo -e "\033[1m\033[4mDone\033[0m"
 echo -e "\033[1m\033[4mNext Steps\033[0m"
 echo -e "\033[32mcd {{cookiecutter.project_name}}\033[0m"
-echo -e "\033[32msource venv/bin/activate\033[0m"
-echo -e "\033[32m{{cookiecutter.cli_name}} --help\033[0m"
+#echo -e "\033[32msource venv/bin/activate\033[0m"
+#echo -e "\033[32m{{cookiecutter.cli_name}} --help\033[0m"
